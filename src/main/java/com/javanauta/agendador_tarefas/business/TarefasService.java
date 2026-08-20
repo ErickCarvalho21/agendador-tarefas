6f8ecab1efe0444cfc5846315aa68f2af37c5d4d
Erick <erick.javaspringcount@gmail.com>
2026-08-19 22:21:53 -0300

    add dockerfile e alterações necessárias

--- src/main/java/com/javanauta/agendador_tarefas/business/TarefasService.java
+++ src/main/java/com/javanauta/agendador_tarefas/business/TarefasService.java
@@ -37,7 +37,8 @@ public class TarefasService {
     public List<TarefasDTO> buscarTarefasAgendadasPorPeriodo(LocalDateTime dataInicial, LocalDateTime dataFinal) {
 
         return tarefaConverter.paraListaTarefasDTO(
-                tarefasRepository.findByDataEventoBetween(dataInicial, dataFinal));
+                tarefasRepository.findByDataEventoBetweenAndStatusNotificacaoEnum(dataInicial, dataFinal,
+                        StatusNotificacaoEnum.PEDENTE));
     }
 
     public List<TarefasDTO> buscarTarefasPorEmail(String token) {
